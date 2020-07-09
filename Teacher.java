a6cb44b76d3d450ae51552b1463c8cf10657f3b0
ÅÓÉÆÌì <pangst@eastcom-sw.com>
2020-07-09 15:31:02 +0800

    update Student file

--- Teacher.java
+++ Teacher.java
@@ -1,3 +1,13 @@
 class Teacher{
   public Teacher(){}
+  String name;
+
+  public String getName() {
+    return name;
+  }
+
+  // set方法
+  public void setName(String name) {
+    this.name = name;
+  }
 }
